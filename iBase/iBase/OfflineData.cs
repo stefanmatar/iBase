802f656228a817d68a72df01869552f1f0284734
dschonas <jonasneumair@gmail.com>
2016-04-17 15:03:44 +0200

    does it work?

---
+++ iBase/iBase/OfflineData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iBase {
+    class OfflineData {
+
+        iBaseDB db = new iBaseDB();
+
+        public IEnumerable<ArtistTable> AllArt {
+            get {
+                return
+                    (from d in db.ArtistTables
+                     select d).ToList();
+            }
+        }
+        public IEnumerable<AlbumTable> AllAlbs {
+            get {
+                return
+                    (from d in db.AlbumTables
+                     select d).ToList();
+            }
+        }
+        public IEnumerable<TrackTable> AllTracks {
+            get {
+                return
+                    (from d in db.TrackTables
+                     select d).ToList();
+            }
+        }
+    }
+}
