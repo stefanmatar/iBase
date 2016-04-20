c98704dd5003403c7c97ec79a877f85848927a65
Dschonas <jonasneumair@gmail.com>
2016-04-20 11:44:40 +0200

    add -A

---
+++ iBase/iBase/NewItem.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace iBase {
+    /// <summary>
+    /// Interaction logic for NewItem.xaml
+    /// </summary>
+    public partial class NewItem : Window {
+        public NewItem () {
+            InitializeComponent();
+        }
+
+        private void Save_Click (object sender, RoutedEventArgs e) {
+            DialogResult = true;
+        }
+    }
+}
